2570997c00bd8dc8c54a1e6d65ee42f6cf0d36ea
Kovid Agarwal <kovid@protonmail.com>
2021-05-23 18:49:33 +0530

    Postgres-In-Kubernetes

---
+++ server/index.js
@@ -0,0 +1,50 @@
+const keys = require('./keys');
+
+//Express App Setup
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+// Postgres Client setup
+
+const { Pool } = require('pg');
+const pgClient = new Pool({
+	user: keys.pgUser,
+	host: keys.pgHost,
+	database: keys.pgDatabase,
+	password: keys.pgPassword,
+	port: keys.pgPort
+});
+
+pgClient.on('connect', () => {
+	pgClient.query('CREATE TABLE IF NOT EXISTS values (number INT)').catch((err) => console.log(err));
+});
+
+//Express route handlers
+app.get('/', (req, res) => {
+	//console.log('Hi');
+	res.send('Hi');
+});
+
+app.get('/values/all', async (req, res) => {
+	const values = await pgClient.query('SELECT * from values');
+	res.send(values.rows);
+});
+
+app.post('/values', async (req, res) => {
+	const index = req.body.index;
+
+	if (parseInt(index) > 40) {
+		return res.status(422).send('Index too high');
+	}
+	pgClient.query('INSERT INTO values(number) VALUES($1)', [ index ]);
+	res.send({ Working: true });
+});
+
+app.listen(5000, (err) => {
+	console.log('Listening at port 5000');
+});
